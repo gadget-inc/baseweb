350d1bded413420436334ccf699b1d90e5dc4e49
Harry Brundage <harry.brundage@gmail.com>
2020-10-21 16:46:35 -0400

    Fix hacky build script

--- hacky-build.sh
+++ hacky-build.sh
@@ -2,8 +2,10 @@
 set -ex
 WORK_DIR="/Users/airhorns/Code/baseweb-hacks-staging"
 SHA=$(git rev-parse --short HEAD)
-# yarn build
-rm -rf $WORK_DIR/*
+yarn build
+rm -rf $WORK_DIR
+mkdir $WORK_DIR
+cp -r .git $WORK_DIR/
 cp -r dist/ $WORK_DIR
 cd $WORK_DIR
 git checkout -b build-$SHA
