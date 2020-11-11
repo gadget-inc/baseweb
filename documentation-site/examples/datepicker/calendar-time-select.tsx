987a17a2c85949e0b173a2acf9006148d3559397
Harry Brundage <harry.brundage@gmail.com>
2020-11-11 16:49:54 -0500

    Build a8f592b0

--- documentation-site/examples/datepicker/calendar-time-select.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {StatefulCalendar} from 'baseui/datepicker';
-
-export default () => (
-  <StatefulCalendar
-    onChange={({date}) => console.log(date)}
-    timeSelectStart
-  />
-);
--- documentation-site/examples/radio/disabled.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import {Radio, RadioGroup} from 'baseui/radio';
-
-export default () => (
-  <RadioGroup disabled name="disabled" value="1">
-    <Radio value="1">Checked</Radio>
-    <Radio value="2">Unchecked</Radio>
-  </RadioGroup>
-);
